b411d675b434e577416c5f6f65bfcc224f5954bb
JihoPark <hufs71@naver.com>
2024-05-10 22:13:55 +0900

    Update K번째 수.py

--- python/인프런/K번째 수.py
+++ python/인프런/K번째 수.py
@@ -18,3 +18,16 @@ while True:
         break
 
 print(answer)
+
+
+'''
+## 정답:
+
+T = int(input())
+for t in range(T):
+    n, s, e, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    a = a[s-1:e]
+    a.sort()
+    print('#%d %d' %(t+1, a[k-1])) # 출력 형식
+'''
