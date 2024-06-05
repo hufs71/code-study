6c7e132fe1d77c33b181f4dcd9c641024f2ef62e
JihoPark <hufs71@naver.com>
2024-06-05 14:09:20 +0900

    Update 5_01_가장 큰 수.py

--- python/인프런/5_01_가장 큰 수.py
+++ python/인프런/5_01_가장 큰 수.py
@@ -1,3 +1,5 @@
+# 정답 풀이
+
 num, m = map(int, input().split())
 num = list(map(int, str(num)))
 
@@ -12,3 +14,31 @@ if m!=0: # 더 지워야함
 
 res = ''.join(map(str, stack))
 print(res)
+
+
+# 내 폴이
+'''
+mx_idx = seperate.index(max(seperate)) 
+idx = 0
+cnt = 0
+
+if mx_idx == m: 
+    seperate = seperate[mx_idx:]
+
+elif mx_idx > m: # max 앞 숫자가 제거해야 할 숫자보다 많으면
+    tmp = seperate[:mx_idx]
+    while cnt <= m:
+        mx = max(tmp)
+        if seperate[idx] != mx:
+            seperate.pop(idx)
+            cnt += 1
+        else:
+            idx += 1 # 제거한 숫자 없으면 idx += 1
+
+else:
+    while cnt < m:
+        mn = min(seperate)
+        mn_idx = seperate.index(mn)
+        seperate.pop(mn_idx)
+        cnt += 1
+'''
